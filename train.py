0b6fe4783746855d834e05c7ed2b93940277f4bc
Jie Zhang <jiegzhan@gmail.com>
2016-12-07 19:49:49 -0800

    train image classifier with rnn class

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import sys
 import json
 import time
 import tensorflow as tf
+from image_rnn import ImageRNN
 from tensorflow.python.ops import rnn, rnn_cell
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -11,7 +12,7 @@ n_steps = 28 # timesteps
 n_hidden = 128 # hidden layer num of features
 n_classes = 10 # MNIST total classes (0-9 digits)
 
-def RNN(x, weights, biases):
+def rnn_model(x, weights, biases):
 	"""RNN (LSTM or GRU) model for image"""
 	x = tf.transpose(x, [1, 0, 2])
 	x = tf.reshape(x, [-1, n_input])
@@ -22,28 +23,31 @@ def RNN(x, weights, biases):
 	return tf.matmul(outputs[-1], weights) + biases
 
 def train():
+	"""Step 0: load image data"""
 	mnist = input_data.read_data_sets("./data/", one_hot=True)
+
+	"""Step 1: load training parameters"""
 	parameter_file = sys.argv[1]
 	params = json.loads(open(parameter_file).read())
 
+	"""
 	x = tf.placeholder("float", [None, n_steps, n_input])
 	y = tf.placeholder("float", [None, n_classes])
 
 	weights = tf.Variable(tf.random_normal([n_hidden, n_classes]), name='weights')
 	biases = tf.Variable(tf.random_normal([n_classes]), name='biases')
 
-	pred = RNN(x, weights, biases)
+	pred = rnn_model(x, weights, biases)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-	optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate']).minimize(cost)
 
 	correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-	init = tf.initialize_all_variables()
-	saver = tf.train.Saver(tf.all_variables())
+	"""
 
 	with tf.Session() as sess:
-		sess.run(init)
+		rnn = ImageRNN(n_input, n_steps, n_hidden, n_classes)
+
+		sess.run(tf.initialize_all_variables())
 		step = 1
 
 		timestamp = str(int(time.time()))
@@ -52,16 +56,21 @@ def train():
 		checkpoint_prefix = os.path.join(checkpoint_dir, "model")
 		if not os.path.exists(checkpoint_dir):
 			os.makedirs(checkpoint_dir)
+		saver = tf.train.Saver(tf.all_variables())
+
+		optimizer = tf.train.AdamOptimizer(learning_rate=params['learning_rate']).minimize(rnn.cost)
 
 		while step * params['batch_size'] < params['training_iters']:
 			batch_x, batch_y = mnist.train.next_batch(params['batch_size'])
 			# Reshape data to get 28 seq of 28 elements
-			batch_x = batch_x.reshape((params['batch_size'], n_steps, n_input))
-			sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+			# batch_x = batch_x.reshape((params['batch_size'], n_steps, n_input))
+			batch_x = tf.reshape(batch_x, (params['batch_size'], n_steps, n_input))
+			sess.run(optimizer, feed_dict={rnn.input_x: batch_x, rnn.input_y: batch_y})
+			sys.exit()
 			if step % params['display_step'] == 0:
 				path = saver.save(sess, checkpoint_prefix, global_step=step)
-				acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-				loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+				acc = sess.run(rnn.accuracy, feed_dict={rnn.input_x: batch_x, rnn.input_y: batch_y})
+				loss = sess.run(rnn.cost, feed_dict={rnn.input_x: batch_x, rnn.input_y: batch_y})
 				print("Iter " + str(step * params['batch_size']) + ", Loss= " + "{:.6f}".format(loss) + ", Accuracy= " + "{:.5f}".format(acc))
 			step += 1
 		print("Optimization Finished!")
@@ -69,7 +78,7 @@ def train():
 		test_len = 128
 		test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
 		test_label = mnist.test.labels[:test_len]
-		print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
+		print("Testing Accuracy:", sess.run(rnn.accuracy, feed_dict={rnn.input_x: test_data, rnn.input_y: test_label}))
 
 if __name__ == '__main__':
 	train()
